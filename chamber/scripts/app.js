184d2ed3efafd94741d9208f01ce660f389549f2
wadzamurombedzi <wadzanaimurombedzi0@gmail.com>
2025-04-05 23:13:36 +0300

    Week O3

---
+++ chamber/scripts/app.js
@@ -0,0 +1,12 @@
+import { initializeCommon } from "./modules/common.mjs";
+//import { displayEvents } from './modules/events.mjs';
+//import { initializeDirectory } from "chamber/scripts/modules/directory.mjs";
+//import { initializeWeather } from './weather.mjs';
+//import { initializeHome } from './home.mjs';
+
+initializeCommon();
+
+//initializeHome();
+//initializeDirectory();
+//initializeWeather();
+//displayEvents();
